62d0503e221df95ea486dcbb3ba771e23192b36f
STAR COMPUTER BHALKI <STAR COMPUTER BHALKI@192.168.1.7>
2023-05-08 13:54:22 -0700

    char demo

---
+++ B_54/src/chardemo.java
@@ -0,0 +1,21 @@
+
+public class chardemo {
+	public static void main (String[] args) {
+		//assigning single char literally
+		char ch = 'a';
+		System.out.println(ch);
+		
+		//assigning number to char
+		char ch1 = 65;
+		System.out.println(ch1);
+		
+		//assigning unicode to char 
+		char var1 = '\u00a7';
+		System.out.println(var1);
+		
+		//asci code representation
+		byte a = 'A';
+		System.out.println(a);
+	}
+
+}
