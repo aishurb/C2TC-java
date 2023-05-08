f0e2a6e28ac90b266d3d1f0e3ff460a638b54ce4
STAR COMPUTER BHALKI <STAR COMPUTER BHALKI@192.168.1.7>
2023-05-08 14:11:17 -0700

    if else demo

---
+++ B_54/src/ifelsedemo.java
@@ -0,0 +1,14 @@
+
+public class ifelsedemo {
+	public static void main(String[] args) {
+		int age=20;
+		if(age>=18)
+			System.out.println("u are eligible for voting");
+		else
+			System.out.println("sorry we need to wait");
+		
+		System.out.println("if else demostartion");
+	}
+	
+
+}
