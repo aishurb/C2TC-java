7a43e65742425a4bb635dcbc0168c0415017df27
STAR COMPUTER BHALKI <STAR COMPUTER BHALKI@192.168.1.7>
2023-05-08 14:01:54 -0700

    if demo

---
+++ B_54/src/ifdemo.java
@@ -0,0 +1,11 @@
+
+public class ifdemo {
+	public static void main (String[] args) {
+		int x = 5;
+		if (x!= 5)
+			System.out.println("value of x is not 5");
+		System.out.println("value of x is 5");
+			
+	}
+
+}
